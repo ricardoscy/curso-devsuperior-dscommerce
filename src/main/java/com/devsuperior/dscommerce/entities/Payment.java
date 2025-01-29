f81edacdb1b5ebe31a4bf76835afa34e469dbd8b
Ricardo <ricardosac@gmail.com>
2025-01-29 16:38:50 -0300

    Inclusao da entidade Payment, junto com o relacionamento OneToOne com Order

--- src/main/java/com/devsuperior/dscommerce/entities/Payment.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Payment.java
@@ -9,24 +9,19 @@ import lombok.Setter;
 import java.time.Instant;
 
 @Entity
-@Table(name = "tb_order")
+@Table(name = "tb_payment")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Order {
+public class Payment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
-    private OrderStatus status;
 
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private User client;
-
-    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-    private Payment payment;
+    @OneToOne
+    @MapsId
+    private Order order;
 }
